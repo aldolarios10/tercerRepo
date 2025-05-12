a9925b82f5798fb4d6d48a0e8516072781ec9ab6
Aldo Larios <lariosaldo013@gmail.com>
2025-05-11 20:51:49 -0600

    mi primer lanzamiento

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="tercerRepo",                              # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Aldo Larios",                           # Tu nombre
+    author_email="aldolarios013@gmail.com",         # Tu correo electrónico
+    url="https://github.com/aldolario10/tercerRepo",     # URL del proyecto
+)
